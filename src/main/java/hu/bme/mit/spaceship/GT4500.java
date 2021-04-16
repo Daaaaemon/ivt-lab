8a3939481d2eaf4c4c8fa5d099c82031caa9aaff
Nagy Balint <info.balint.nagy@gmail.com>
2021-04-16 18:50:53 +0200

    implement fireLaser with limited ammo capacity

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -7,17 +7,23 @@ public class GT4500 implements SpaceShip {
 
   private TorpedoStore primaryTorpedoStore;
   private TorpedoStore secondaryTorpedoStore;
+  private int battery;
 
   private boolean wasPrimaryFiredLast = false;
 
   public GT4500() {
     this.primaryTorpedoStore = new TorpedoStore(10);
     this.secondaryTorpedoStore = new TorpedoStore(10);
+    this.battery = 100;
   }
 
   public boolean fireLaser(FiringMode firingMode) {
-    // TODO not implemented yet
+    if (battery > 0) {
+        battery--;
+        return true;
+    }
     return false;
+      
   }
 
   /**
